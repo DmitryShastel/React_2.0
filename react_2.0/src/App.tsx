451f5e94165335e517401d00ba3a175c7a209cd2
DmitryShastel <dmshastel@gmail.com>
2022-10-09 11:04:03 +0300

    The React_№3 the component Rating is created

--- react_2.0/src/App.tsx
+++ react_2.0/src/App.tsx
@@ -1,21 +1,22 @@
 import React from 'react';
 import './App.css';
-import {Accordion} from "./components/Accordion/accordion";
-import {Rating} from "./components/Rating/rating";
+import {Accordion} from "./components/Accordion/Accordion";
+import {Rating} from "./components/Rating/Rating";
 
 function App() {
     return (
         <div className="App">
             <AppTitle/>
-            <Rating/>
+            <Rating value={3}/>
             <Accordion/>
+            <Rating value={4}/>
         </div>
     );
 }
 
 const AppTitle = () => {
     return (
-        <>This is APP component</>
+        <h3>This is APP component</h3>
     )
 }
 
