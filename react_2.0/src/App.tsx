1262f34c1d54dbcdee3e41b92133deab591017ff
DmitryShastel <dmshastel@gmail.com>
2022-11-13 11:10:46 +0300

    The function turnOn is created

--- react_2.0/src/App.tsx
+++ react_2.0/src/App.tsx
@@ -2,25 +2,32 @@ import React from 'react';
 import './App.css';
 import {Accordion} from "./components/Accordion/Accordion";
 import {Rating} from "./components/Rating/Rating";
+import {OnOff} from "./components/On_Off/On_Off";
 
 function App() {
     return (
         <div className="App">
-            <PageTitle title={'This is APP component'}/>
-            <Accordion title={'Menu'} collapsed={true}/>
-            <Accordion title={'Users'} collapsed={false}/>
+            <OnOff/>
 
-            <Rating value={0}/>
-            <Rating value={1}/>
-            <Rating value={2}/>
-            <Rating value={3}/>
-            <Rating value={4}/>
-            <Rating value={5}/>
+            {/*<PageTitle title={'This is APP component'}/>*/}
+            {/*<Accordion title={'Menu'} collapsed={true}/>*/}
+            {/*<Accordion title={'Users'} collapsed={false}/>*/}
+
+            {/*<Rating value={0}/>*/}
+            {/*<Rating value={1}/>*/}
+            {/*<Rating value={2}/>*/}
+            {/*<Rating value={3}/>*/}
+            {/*<Rating value={4}/>*/}
+            {/*<Rating value={5}/>*/}
 
 
         </div>
 
     );
+}
+
+const turnOn = (event: React.FormEvent<HTMLFormElement>) =>{
+    
 }
 
 
--- react_2.0/src/components/On_Off/On_Off.tsx
+++ react_2.0/src/components/On_Off/On_Off.tsx
@@ -3,7 +3,9 @@ import React from 'react';
 export const OnOff = () => {
     return (
         <div>
-
+            <button onClick={() => {}}>on</button>
+            <button onClick={() => {}}>off</button>
+            <input type='color'/>
         </div>
     );
 };
