3fbdfdaaf84796394e46270ac4ee5dabd01d5039
DmitryShastel <dmshastel@gmail.com>
2023-01-25 21:35:33 +0300

    The Rating is done

--- react_2.0/src/App.tsx
+++ react_2.0/src/App.tsx
@@ -3,13 +3,13 @@ import './App.css';
 import {Rating, RatingValueType} from "./components/Rating/Rating";
 
 function App() {
-    let [ratingValue, setRatingvalue] = useState<RatingValueType>(0)
+    let [ratingValue, setRatingValue] = useState<RatingValueType>(0)
 
     return (
         <div className="App">
 
 
-            <Rating value={ratingValue} onClick={setRatingvalue}/>
+            <Rating value={ratingValue} onClick={setRatingValue}/>
 
 
             {/*<UncontrolledAccordion title={'Menu'}/>*/}
