e256039f7efc3e9062fb155fba99c00bb86969eb
DmitryShastel <dmshastel@gmail.com>
2023-01-26 08:37:12 +0300

    The UncontrolledRating is done

--- react_2.0/src/components/UncontrolledRating/UncontrolledRating.tsx
+++ react_2.0/src/components/UncontrolledRating/UncontrolledRating.tsx
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 
 type  RatingType = {
-    value: 0 | 1 | 2 | 3 | 4 | 5
+
 }
 
 
@@ -10,11 +10,11 @@ export const UncontrolledRating = (props: RatingType) => {
     let [value, setValue] = useState(0);
     return (
         <div>
-            <Star selected={value > 0} setValue={() => {setValue(1)}} value={1}/>
-            <Star selected={value > 1} setValue={() => {setValue(2)}} value={2}/>
-            <Star selected={value > 2} setValue={() => {setValue(3)}} value={3}/>
-            <Star selected={value > 3} setValue={() => {setValue(4)}} value={4}/>
-            <Star selected={value > 4} setValue={() => {setValue(5)}} value={5}/>
+            <Star selected={value > 0} setValue={() => {setValue(1)}}/>
+            <Star selected={value > 1} setValue={() => {setValue(2)}}/>
+            <Star selected={value > 2} setValue={() => {setValue(3)}}/>
+            <Star selected={value > 3} setValue={() => {setValue(4)}}/>
+            <Star selected={value > 4} setValue={() => {setValue(5)}}/>
         </div>
     )
 }
@@ -22,14 +22,14 @@ export const UncontrolledRating = (props: RatingType) => {
 
 type StarPropsType = {
     selected: boolean
-    value: 0 | 1 | 2 | 3 | 4 | 5
-    setValue: (value: 0 | 1 | 2 | 3 | 4 | 5) => void
+
+    setValue: () => void
 }
 
 const Star = (props: StarPropsType) => {
 
     return <span onClick={() => {
-        props.setValue(props.value)
+        props.setValue()
     }}>
     {props.selected ? <b> Star </b> : 'star '}
     </span>
