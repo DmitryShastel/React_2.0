bc78d1351595116c62b495ae2d6b610dedeb2bc1
DmitryShastel <dmshastel@gmail.com>
2023-01-29 10:58:50 +0300

    The useState is created

--- react_2.0/src/App.tsx
+++ react_2.0/src/App.tsx
@@ -3,10 +3,12 @@ import './App.css';
 import {Rating, RatingValueType} from "./components/Rating/Rating";
 import {UncontrolledRating} from "./components/UncontrolledRating/UncontrolledRating";
 import {Accordion} from "./components/Accordion/Accordion";
+import {UncontrolledOnOff} from "./components/UncontrolledOn_Off/On_Off";
 
 function App() {
     let [ratingValue, setRatingValue] = useState<RatingValueType>(0)
     let [accordionCollapsed, setAccordionCollapsed] = useState<boolean>(false)
+    let [switchOn, setSwitchOn] = useState<boolean>(false)
 
     return (
         <div className="App">
@@ -14,10 +16,12 @@ function App() {
             <Rating value={ratingValue} onClick={setRatingValue}/>
             <UncontrolledRating/>
 
-            <Accordion
-                title={'Menu'}
-                collapsed={accordionCollapsed}
-                onChange={() => {setAccordionCollapsed(!accordionCollapsed)}}/>
+            <UncontrolledOnOff/>
+
+            {/*<Accordion*/}
+            {/*    title={'Menu'}*/}
+            {/*    collapsed={accordionCollapsed}*/}
+            {/*    onChange={() => {setAccordionCollapsed(!accordionCollapsed)}}/>*/}
 
             {/*<UncontrolledAccordion title={'Menu'}/>*/}
             {/*<UncontrolledAccordion title={'Users'}/>*/}
