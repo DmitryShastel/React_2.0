d6b843f9e38cd2e5ac92f7c41305e5c89fa7c89c
unknown <dmshastel@gmail.com>
2023-05-14 09:35:31 +0300

    The React#10 OnOff stories is added

---
+++ react_2.0/src/Tuesday/Stories/OnOff.stories.tsx
@@ -0,0 +1,13 @@
+import {OnOff} from "../../Monday/components/On_Off/On_Off";
+
+
+export default {
+    title: 'OnOff stories',
+    component: OnOff
+}
+
+export const OnOff2 = () => {
+    return(
+        <div>hello</div>
+    )
+}
