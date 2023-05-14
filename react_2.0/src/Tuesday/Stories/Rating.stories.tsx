d9323daa7e915d7140de59681abd467529a4d9bd
unknown <dmshastel@gmail.com>
2023-05-14 09:09:02 +0300

    The React#10 Rating componenet is created

---
+++ react_2.0/src/Tuesday/Stories/Rating.stories.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Rating} from "../../Monday/components/Rating/Rating";
+
+export default {
+    title: 'Rating',
+    component: Rating
+}
+
+export const Test = () => {
+    return (
+        <div></div>
+    )
+}
