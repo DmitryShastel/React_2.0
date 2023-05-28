94f05514ae0dd4b89c65358605dcf158e780f535
unknown <dmshastel@gmail.com>
2023-05-28 13:27:33 +0300

    The React#11 MenuCollapsedMode2/UnCollapsedMode2 is added

--- react_2.0/src/Tuesday/Stories/Accordion.stories.tsx
+++ react_2.0/src/Tuesday/Stories/Accordion.stories.tsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import {Accordion, AccordionType} from "../../Monday/components/Accordion/Accordion";
 import {action} from "@storybook/addon-actions";
+import {Story} from "@storybook/react";
 
 export default {
     title: 'Accordion',
@@ -10,14 +11,33 @@ export default {
 const callback = action('accordion mode change event fired')
 const onClickCallback = action('some item was clicked')
 
+const Template: Story<AccordionType> = (args) => <Accordion {...args}/>
+
+export const MenuCollapsedMode2 = Template.bind({})
+MenuCollapsedMode2.args = {
+    title: 'test title',
+    collapsed: true,
+    onClick: onClickCallback,
+    onChange: callback
+}
+
+export const UnCollapsedMode2 = Template.bind({})
+UnCollapsedMode2.args= {
+    title: 'test title',
+    collapsed: false,
+    onClick: onClickCallback,
+    onChange: callback
+}
 
 export const CollapsedMode = () => <Accordion
+    // items={[]}
     title={'test title'}
     collapsed={true}
     onClick={onClickCallback}
     onChange={callback}/>
 
-export const UncollapsedMode = () => <Accordion
+export const UnCollapsedMode = () => <Accordion
+    // items={[]}
     title={'test title'}
     collapsed={false}
     onClick={onClickCallback}
