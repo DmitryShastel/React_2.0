71216f3258b1b5a520139ee280be2d3a29087d13
unknown <dmshastel@gmail.com>
2023-05-28 09:50:51 +0300

    The React#10 is done

--- react_2.0/src/Tuesday/Stories/UncontrolledRating.stories.tsx
+++ react_2.0/src/Tuesday/Stories/UncontrolledRating.stories.tsx
@@ -7,12 +7,13 @@ export default {
     component: UncontrolledRating
 }
 
-export const EmptyRating = () => <Rating value={0} onClick={x => x}/>
-export const Rating1 = () => <Rating value={1} onClick={x => x}/>
-export const Rating2 = () => <Rating value={2} onClick={x => x}/>
-export const Rating3 = () => <Rating value={3} onClick={x => x}/>
-export const Rating4 = () => <Rating value={4} onClick={x => x}/>
-export const Rating5 = () => <Rating value={5} onClick={x => x}/>
+export const EmptyRating = () => <UncontrolledRating />
+// export const Rating1 = () => <UncontrolledRating value={1} onClick={x => x}/>
+// export const Rating2 = () => <UncontrolledRating value={2} onClick={x => x}/>
+// export const Rating3 = () => <UncontrolledRating value={3} onClick={x => x}/>
+// export const Rating4 = () => <UncontrolledRating value={4} onClick={x => x}/>
+// export const Rating5 = () => <UncontrolledRating value={5} onClick={x => x}/>
+
 export const RatingChanging = () => {
     const [rating, setRating] = useState<RatingValueType>(3)
     return <Rating value={rating} onClick={setRating}/>
