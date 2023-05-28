dd1ab5887c6ecd3e6ad574847d19c7cf98ced4e9
unknown <dmshastel@gmail.com>
2023-05-28 10:24:35 +0300

    The React#11 callback is added

--- react_2.0/src/Tuesday/Stories/Accordion.stories.tsx
+++ react_2.0/src/Tuesday/Stories/Accordion.stories.tsx
@@ -1,17 +1,20 @@
 import React, {useState} from 'react';
 import {Accordion, AccordionType} from "../../Monday/components/Accordion/Accordion";
+import {action} from "@storybook/addon-actions";
 
 export default {
     title: 'Accordion',
     component: Accordion
 }
 
+const callback = action('accordion mode change event fired')
 
-export const CollapsedMode  = () => <Accordion title={'test title'} collapsed={true} onChange={()=> {}}/>
+
+export const CollapsedMode  = () => <Accordion title={'test title'} collapsed={true} onChange={callback}/>
 export const UncollapsedMode  = () => <Accordion title={'test title'} collapsed={false} onChange={()=> {}}/>
 
 export const CombinedAccordion = () => {
-    let [accordionCollapsed, setAccordionCollapsed] = useState<any>(false)
+    let [accordionCollapsed, setAccordionCollapsed] = useState<boolean>(false)
 
     return <Accordion title={'test title'} collapsed={accordionCollapsed} onChange={()=> setAccordionCollapsed(!accordionCollapsed)}/>
 
