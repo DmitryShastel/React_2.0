caf7a6da5ed7e1ea506d36a15be6ba5ddc3aa502
Dmitry Shastel <dmshastel@gmail.com>
2023-06-08 16:46:40 +0300

    The React#14 getDisplay is created

--- react_2.0/src/Tuesday/Select/Dropdown.tsx
+++ react_2.0/src/Tuesday/Select/Dropdown.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
+import {Icon} from "./Icon";
 
+
+type PlaceHolderType = {
+    placeHolder: any
+}
 
 //@ts-ignore
-export const Dropdown = ({placeHolder: any}) => {
-    return (
-        <div>
+export const Dropdown = (props: PlaceHolderType) => {
 
+    const getDisplay = () => {
+        return props.placeHolder
+    }
+
+    return (
+        <div className="dropdown-container">
+            <div className="dropdown-input">
+                <div className="dropdown-selected-value">{getDisplay()}</div>
+                <div className="dropdown-tools">
+                    <div className="dropdown-tool">
+                        <Icon/>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
+
+
