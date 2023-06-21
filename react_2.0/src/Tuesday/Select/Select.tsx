eabbcf5576c7655628a15a5248a4393c6540fadb
Dmitry Shastel <dmshastel@gmail.com>
2023-06-21 17:44:32 +0300

    The React#14 Select.module.css is created

--- react_2.0/src/Tuesday/Select/Select.tsx
+++ react_2.0/src/Tuesday/Select/Select.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import s from './Select.module.css'
 
 type ItemType = {
     title: string
@@ -17,11 +18,18 @@ export const Select = (props: SelectPropsType) => {
     const selectedItem = props.items.find(i => i.value === props.value)
 
     return (
+        <>
+            <select>
+                <option value="">Minsk</option>
+                <option value="">Moscow</option>
+                <option value="">Kiev</option>
+            </select>
+            <div className={s.select}>
+                <h3>{selectedItem && selectedItem.title}</h3>
+                {props.items.map(i => <div key={i.value}>{i.title}</div>)}
+            </div>
 
-        <div>
-            <h3>{selectedItem && selectedItem.title}</h3>
-            {props.items.map(i => <div key={i.value}>{i.title}</div>)}
-        </div>
+        </>
 
     );
 };
