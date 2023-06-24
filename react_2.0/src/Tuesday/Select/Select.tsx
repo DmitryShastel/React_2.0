1d151be19c871a2dd57fc0968e9b2f0d4a4f3ca6
unknown <dmshastel@gmail.com>
2023-06-24 09:24:56 +0300

    The React#14 onKeyUp is changed

--- react_2.0/src/Tuesday/Select/Select.tsx
+++ react_2.0/src/Tuesday/Select/Select.tsx
@@ -32,14 +32,19 @@ export const Select = (props: SelectPropsType) => {
     }
 
     const onKeyUp = (e: KeyboardEvent<HTMLDivElement>) => {
-        for (let i = 0; i < props.items.length; i++) {
-            if (props.items[i].value === hoveredElementValue) {
-                if (props.items[i + 1]) {
-                    props.onChange(props.items[i + 1].value)
-                    break
+        if (e.key === 'ArrowDown' || e.key === 'ArrowUp') {
+            for (let i = 0; i < props.items.length; i++) {
+                if (props.items[i].value === hoveredElementValue) {
+                    const pretendentElement = e.key === 'ArrowDown' ? props.items[i + 1] : props.items[i - 1]
+
+                    if (pretendentElement) {
+                        props.onChange(pretendentElement.value)
+                        break
+                    }
                 }
             }
         }
+
     }
 
 
