b36afef5d55044997063c4459437895821669a30
Dmitry Shastel <dmshastel@gmail.com>
2023-06-27 14:24:13 +0300

    The React#17 is done

--- react_2.0/src/Tuesday/Stories/ReactMemo.stories.tsx
+++ react_2.0/src/Tuesday/Stories/ReactMemo.stories.tsx
@@ -9,17 +9,27 @@ const NewMessagesCounter = (props: { count: number }) => {
     return <div>{props.count}</div>
 }
 
-const Users = (props: { users: Array<string> }) => {
+const UsersSecret = (props: { users: Array<string> }) => {
+    console.log('users')
     return <div>
         {props.users.map((u, i) => <div key={i}>{u}</div>)}
     </div>
 }
 
+const Users = React.memo(UsersSecret)
+
 export const Example1 = () => {
     const [counter, setCounter] = useState(0)
     const [users, setUsers] = useState(['Dima', 'Valera'])
 
+    const addUser = () => {
+        const newUser = [...users, 'Sveta' + new Date().getMinutes()]
+        setUsers(newUser)
+    }
+
     return <>
+        <button onClick={() => setCounter(counter + 1)}>+</button>
+        <button onClick={addUser}>add user</button>
         <NewMessagesCounter count={counter}/>
         <Users users={users}/>
     </>
