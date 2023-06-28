741327b109e55162c8f7ecaf16e62a3b54f02bc1
Dmitry Shastel <dmshastel@gmail.com>
2023-06-28 19:01:09 +0300

    The React#18 is done

--- react_2.0/src/Tuesday/Stories/UseMemo.stories.tsx
+++ react_2.0/src/Tuesday/Stories/UseMemo.stories.tsx
@@ -66,10 +66,16 @@ export const HelpsToReactMemo = () => {
     const newArray = useMemo(() => {
         const newArray = users.filter(u => u.toLowerCase().indexOf('a') > -1)
         return newArray
-    },[])
+    }, [users])
+
+    const addUser = () => {
+        const newUsers = [...users, 'Sveta' + new Date().getTime()]
+        setUsers(newUsers)
+    }
 
     return <>
         <button onClick={() => setCounter(counter + 1)}>+</button>
+        <button onClick={() => addUser()}>add user</button>
         {counter}
         <Users users={newArray}/>
     </>
