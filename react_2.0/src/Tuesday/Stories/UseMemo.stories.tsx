ff2f84c4a8f6a4f3daf6e4e8da41efa576de593f
Dmitry Shastel <dmshastel@gmail.com>
2023-06-28 18:26:37 +0300

    The React#18 UseMemo is added

--- react_2.0/src/Tuesday/Stories/UseMemo.stories.tsx
+++ react_2.0/src/Tuesday/Stories/UseMemo.stories.tsx
@@ -1,7 +1,7 @@
-import React, {useState} from 'react';
+import React, {useMemo, useState} from 'react';
 
 export default {
-    title: 'React.memo',
+    title: 'UseMemo',
 }
 
 
@@ -12,6 +12,19 @@ export const Example1 = () => {
 
     let resultA = 1
     let resultB = 1
+
+    resultA = useMemo(() => {
+        let tempResultA = 1;
+        for (let i = 1; i <= a; i++) {
+            let fake = 0;
+            while (fake < 100000000) {
+                fake++;
+            }
+            tempResultA = tempResultA * i
+        }
+        return tempResultA;
+    }, [a])
+
 
     for (let i = 1; i <= a; i++) {
         resultA = resultA * i
@@ -26,10 +39,10 @@ export const Example1 = () => {
         <input value={b} onChange={(e) => setB(+e.currentTarget.value)}/>
         <hr/>
         <div>
-            Result a: {a}
+            Result a: {resultA}
         </div>
         <div>
-            Result b: {b}
+            Result b: {resultB}
         </div>
     </>
 }
